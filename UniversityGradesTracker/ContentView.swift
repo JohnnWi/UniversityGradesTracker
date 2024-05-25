586ce9f5b42eaca2c86c97e0d7b99f88f8b7736c
Finalyxre <andreacipriani2003@gmail.com>
2024-05-25 19:16:53 +0200

    Initial Commit

---
+++ UniversityGradesTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UniversityGradesTracker
+//
+//  Created by Andrea Cipriani on 25/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UniversityGradesTracker/UniversityGradesTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversityGradesTrackerApp.swift
+//  UniversityGradesTracker
+//
+//  Created by Andrea Cipriani on 25/05/24.
+//
+
+import SwiftUI
+
+@main
+struct UniversityGradesTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
